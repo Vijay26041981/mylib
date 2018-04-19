a05dfc7efb236130ca08b0c6c969b16ffc300a74
Vijay26041981 <38313568+Vijay26041981@users.noreply.github.com>
2018-04-19 15:49:38 +0530

    mylib1

--- jetlib/src/main/java/com/example/jetlib/jetstring.java
+++ jetlib/src/main/java/com/example/jetlib/jetstring.java
@@ -1,6 +1,6 @@
 package com.example.jetlib;
 
 public class jetstring {
-    public static final String pwtrep = "http://103.94.27.90/sdphp//clientupdate.php";
-    public static final String pwtrep1 = "http://103.94.27.90/sdphp//clientupdate.php";
+    public static final String llochk1 = "http://103.94.27.90/sdphp/lochk1.php";
+    
 }
