356abbdadb923d5d18d9932e8700570a25c6d88d
Vijay26041981 <38313568+Vijay26041981@users.noreply.github.com>
2018-04-19 15:56:48 +0530

    mylib1

--- jetlib/src/main/java/com/example/jetlib/jetstring.java
+++ jetlib/src/main/java/com/example/jetlib/jetstring.java
@@ -1,6 +1,28 @@
 package com.example.jetlib;
 
 public class jetstring {
-    public static final String llochk = "http://103.94.27.90/sdphp/lochk1.php";
-
+   
+    public static final String dbfupdate = "http://103.94.27.90/sdphp//clientupdate.php";
+    public static final String dbfupdate1 = "http://103.94.27.90/sdphp//gameupdate.php";
+    public static final String dbfupdate2 = "http://103.94.27.90/sdphp//rateupdate.php";
+    public static final String enterrep = "http://103.94.27.90/sdphp//entreport.php";
+    public static final String pwtrep = "http://103.94.27.90/sdphp//pwtreport.php";
+    public static final String summrep = "http://103.94.27.90/sdphp//summary.php";
+    public static final String csummrep = "http://103.94.27.90/sdphp//csummary.php";
+    public static final String chkclientname = "http://103.94.27.90/sdphp//clientcheck.php";
+    public static final String chkupdate = "http://103.94.27.90/sdphp//clupdate.php";
+    public static final String downduke = "http://http://103.94.27.90/sdphp/duke.txt";
+    public static final String downsklex = "http://103.94.27.90/sdphp/sklex.TXT";
+    public static final String entreport = "http://103.94.27.90/sdphp/entreport.php";
+    public static final String entdele = "http://103.94.27.90/sdphp/entdele.php";
+    public static final String llochk = "http://103.94.27.90/sdphp/lochk.php";
+    public static final String llochksms = "http://103.94.27.90/sdphp/lochksms.php";
+    public static final String mainlist = "http://103.94.27.90/sdphp/mainlist.php";
+    public static final String gpwtreport = "http://103.94.27.90/sdphp/gpwtreport.php";
+    public static final String srateup = "http://103.94.27.90/sdphp/srateup.php";
+    public static final String ggetres = "http://103.94.27.90/sdphp/getres.php";
+    public static final String llochk1 = "http://103.94.27.90/sdphp/lochk1.php";
+    public static final String clientlist = "http://103.94.27.90/sdphp/clientlist.php";
+    public static final String ssduke = "http://103.94.27.90/sdphp/duke.txt";
+    public static final String lochkupp = "http://103.94.27.90/sdphp/lochkup.php";
 }
